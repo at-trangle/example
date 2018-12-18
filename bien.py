3964be3f5eb59e9726724a537cbdb1acd5256db0
at-trangle <trang.le2@asiantech.com>
2018-12-18 14:02:31 +0700

    second commit

---
+++ bien.py
@@ -0,0 +1,15 @@
+a = 2
+b = "xin chao"
+x = [ 1.1, 'abc', 'a', 5, 3.2]
+print a
+print (b)
+# print x[0]
+# for i in x : print i
+sum = a + x[3]
+string = b + " " + x[1]
+abs = x[4] - x[0]
+
+print sum, string, abs
+
+chiadu = x[3]%a
+print chiadu
